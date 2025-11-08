bee344a5ba441f9942af63105aba8341d6872b2b
Mostafa <mostafaamr99533@gmail.com>
2025-11-08 20:29:21 +0200

    + Added custom_text_form. + Added reset_password. + Added assets and utils. + changes main.

---
+++ lib/home/widgets/custom_text_form.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../utils/app_colors.dart';
+import '../utils/app_styles.dart';
+
+class CustomTextForm extends StatelessWidget {
+  Color borderSideColor;
+  String? hintText;
+  TextStyle? hintStyle;
+  Widget? prefixIcon;
+  Widget? suffixIcon;
+  CustomTextForm({super.key,
+    this.borderSideColor = AppColors.greyColor,
+    this.hintStyle , required this.hintText,
+    this.prefixIcon , this.suffixIcon});
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding:  EdgeInsets.symmetric(horizontal: width*0.03 ),
+      child: TextFormField(
+          cursorColor: AppColors.whiteColor,
+          style: TextStyle(
+            color: AppColors.whiteColor,
+            fontSize: 18,
+          ),
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: AppColors.greyColor,
+            enabledBorder: builtDecorationBorder(borderSideColor:borderSideColor ),
+            focusedBorder: builtDecorationBorder(borderSideColor: borderSideColor),
+            errorBorder: builtDecorationBorder(borderSideColor: AppColors.redColor),
+            focusedErrorBorder: builtDecorationBorder(borderSideColor: AppColors.redColor),
+            hintText: hintText,
+            hintStyle: hintStyle ?? AppStyles.medium14White ,
+            prefixIcon: prefixIcon,
+            suffixIcon:suffixIcon ,
+          )
+      ),
+    );
+  }
+}
+OutlineInputBorder builtDecorationBorder({ required Color borderSideColor}){
+  return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide(
+          color: borderSideColor
+      )
+  );
+}
