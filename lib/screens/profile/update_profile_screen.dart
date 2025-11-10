37341e4262cd40357eccc2709eba3debefc96a55
DRAGON <mahmoudessam936@gmail.com>
2025-11-10 17:17:11 +0200

    files image

---
+++ lib/screens/profile/update_profile_screen.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+
+import '../../core/app_colors.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: const ProfileBody(),
+      bottomNavigationBar: const ProfileBottomNav(),
+    );
+  }
+}
+
+class ProfileBody extends StatelessWidget {
+  const ProfileBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            color: Colors.black,
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Column(
+              children: [
+                Padding(padding: EdgeInsets.all(50),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Image.asset(
+                          'lib/assets/edit/gamer.png',
+                          width: 118,
+                          height: 118,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'John Safwat',
+                          style: TextStyle(
+                            color: AppColors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 30,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: const [
+                            Text(
+                              '12',
+                              style: TextStyle(
+                                color: AppColors.white,
+                                fontSize: 36,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              'Wish List',
+                              style: TextStyle(fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.white),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(width: 50),
+                        Column(
+                          children: const [
+                            Text(
+                              '10',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 36,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              'History',
+                              style: TextStyle(fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                ),
+
+                const SizedBox(height: 16),
+
+                const SizedBox(height: 20),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.amber,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 75,
+                          vertical: 15,
+                        ),
+                      ),
+                      child: Text(
+                        'Edit Profile',
+                        style: TextStyle(fontSize: 20,
+                            color: AppColors.black),
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 30,
+                          vertical: 12,
+                        ),
+                      ),
+                      child: const Text(
+                        'Exit',
+                        style: TextStyle(fontSize: 20,
+                            color: AppColors.white),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Text(
+                      'Watch List',
+                      style: TextStyle(color: Colors.amber, fontSize: 16),
+                    ),
+                    Text(
+                      'History',
+                      style: TextStyle(color: Colors.white70, fontSize: 16),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 60),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileBottomNav extends StatelessWidget {
+  const ProfileBottomNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: Colors.black,
+      selectedItemColor: Colors.amber,
+      unselectedItemColor: Colors.white70,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Wish List'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+      ],
+      currentIndex: 2,
+      onTap: (index) {},
+    );
+  }
+}
