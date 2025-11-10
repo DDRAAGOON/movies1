05000c5e3a5f8dc9dc2fd9287ed75a107846be6b
DRAGON <mahmoudessam936@gmail.com>
2025-11-10 17:17:11 +0200

    files image

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color(0xFFFFBB3B);
+  static const Color yellow = Color(0xFFF6BD00);
   static const Color black = Color(0xFF121312);
   static const Color gray = Color(0xFF282A28);
   static const Color white = Color(0xFFFFFFFF);
